a27cdaaf0231a283db3312f89e151a9032e76793
elhamhsnpr <elham.hassanpour133@gmail.com>
2024-05-07 20:01:19 +0200

    add addNote functionlity and load and save functions

--- notes-app/notes.js
+++ notes-app/notes.js
@@ -1,5 +1,42 @@
+const fs = require('fs')
+
+const addNote = function (title, body) {
+    const notes = loadNotes()
+    const duplicateNotes = notes.filter(function (note) {
+        return note.title === title
+    })
+
+    if (duplicateNotes.length === 0) {
+        notes.push({
+            title: title,
+            body: body
+        })
+        saveNotes(notes)
+        console.log('New note added!')
+    } else {
+        console.log('Note title taken!')
+    }
+
+}
+
 const getNotes = function () {
     return "Your notes..."
 }
+const saveNotes = function (notes) {
+    const dataJson = JSON.stringify(notes)
+    fs.writeFileSync('notes.json', dataJson)
+}
 
-module.exports = getNotes
+const loadNotes = function () {
+    try {
+        const dataBuffer = fs.readFileSync('notes.json')
+        const dataJson = dataBuffer.toString()
+        return JSON.parse(dataJson)
+    } catch (e) {
+        return []
+    }
+}
+
+module.exports = {
+    addNote: addNote
+}
